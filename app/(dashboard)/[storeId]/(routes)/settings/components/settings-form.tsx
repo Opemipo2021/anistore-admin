5c4b1a58cbdc1e9b0d9d02a33cb32e4f11ef996c
Opemipo20201 <omoimole2020@gmail.com>
2023-07-22 22:28:15 +0100

    Added Badge and Api Section

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -15,6 +15,7 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/ui/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
 
 interface SettingsFormProps{
     initialData: Store;
@@ -114,7 +115,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                 </Button>
             </form>
         </Form>
-
+        <Separator/>
+        <ApiAlert 
+            title="NEXT_PUBLIC_API_URL" 
+            description="test-desc" 
+            variant="public"
+            />
         </>
     )
 }
